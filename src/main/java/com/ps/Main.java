c6126c2afaad036c319072e99c4cb0ab8627ffa0
Wai_Yan <waiyanbhonemyint2002@gmail.com>
2025-04-18 23:43:57 -0400

    Project done

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,47 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    static String [] nameParts = new String[3];
+
     public static void main(String[] args) {
+
+        System.out.println("Welcome to Full Name Parser");
+
+        System.out.println("Please enter your name: ");
+        String fullName = scanner.nextLine();
+
+        nameSplitter(fullName);
+        displayResult();
+
+    }
+
+    //-------------------Helping Method--------------------//
+    public static void nameSplitter(String fullName){
+
+        String trimFullName = fullName.trim();
+        String cleanFullName = trimFullName.replaceAll("\\s+", " ");
+        nameParts = cleanFullName.split(" ");
+    }
+
+    public static void  displayResult(){
+
+        String firstName = "";
+        String middleName = "";
+        String lastName = "";
+
+        if (nameParts.length<3){
+            System.out.println("firstName = "+ nameParts[0]);
+            System.out.println("middleName = (none)");
+            System.out.println("lastName = " + nameParts[1]);
+        }
+        else{
+            System.out.println("firstName = " + nameParts[0]);
+            System.out.println("middleName = " + nameParts[1]);
+            System.out.println("lastName = " + nameParts[2]);
+        }
 
     }
 }
